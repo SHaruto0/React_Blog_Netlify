f8a3c85b2c54c70b9356072188be15d37da04f3d
SHaruto0 <szkharuto0407@gmail.com>
2024-09-19 10:26:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: "/react_blog_netlify/", // Your repository name goes here
 });
