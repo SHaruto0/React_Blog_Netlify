dd334d17b136b3d142ca11cadfde839f8e27d8cf
SHaruto0 <79863624+SHaruto0@users.noreply.github.com>
2024-09-30 00:42:42 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react_blog_netlify/", // Your repository name goes here
 });
